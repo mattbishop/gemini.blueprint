e8576d18838371c1813577fbcf6473b4fe706d47
Glyn Normington <gnormington@vmware.com>
2012-08-24 15:10:40 +0100

    delete all but one integration test and make that test sysout/err

--- integration-tests/tests/src/test/java/org/eclipse/gemini/blueprint/iandt/cardinality0to1/Cardinality0to1Test.java
+++ integration-tests/tests/src/test/java/org/eclipse/gemini/blueprint/iandt/cardinality0to1/Cardinality0to1Test.java
@@ -31,72 +31,14 @@ import org.eclipse.gemini.blueprint.iandt.cardinality0to1.test.MyListener;
 import org.eclipse.gemini.blueprint.iandt.cardinality0to1.test.ReferenceContainer;
 import org.eclipse.gemini.blueprint.service.ServiceUnavailableException;
 
-/**
- * @author Hal Hildebrand Date: Dec 6, 2006 Time: 6:04:42 PM
- */
 public class Cardinality0to1Test extends BaseIntegrationTest {
 
-	protected String[] getTestBundlesNames() {
-		return new String[] { "org.eclipse.gemini.blueprint.iandt, simple.service," + getSpringDMVersion(),
-			"org.eclipse.gemini.blueprint.iandt, cardinality0to1," + getSpringDMVersion() };
-	}
+	public void testSystemOutErr() throws Exception {
 
-	public void test0to1Cardinality() throws Exception {
-		Bundle simpleService2Bundle = bundleContext.installBundle(getLocator().locateArtifact(
-			"org.eclipse.gemini.blueprint.iandt", "simple.service2", getSpringDMVersion()).getURL().toExternalForm());
-
-		assertNotNull("Cannot find the simple service 2 bundle", simpleService2Bundle);
-
-		assertNotSame("simple service 2 bundle is in the activated state!", new Integer(Bundle.ACTIVE), new Integer(
-			simpleService2Bundle.getState()));
-
-		assertEquals("Unxpected initial binding of service", 0, MyListener.BOUND_COUNT);
-		assertEquals("Unexpected initial unbinding of service", 1, MyListener.UNBOUND_COUNT);
-		assertNotNull("Service reference should be not null", ReferenceContainer.service);
-
-		try {
-			ReferenceContainer.service.stringValue();
-			fail("Service should be unavailable");
-		}
-		catch (ServiceUnavailableException e) {
-			// expected
-		}
-
-		startDependency(simpleService2Bundle);
-
-		assertEquals("Expected initial binding of service", 1, MyListener.BOUND_COUNT);
-		assertEquals("Unexpected initial unbinding of service", 1, MyListener.UNBOUND_COUNT);
-		assertNotNull("Service reference should be not null", ReferenceContainer.service);
-
-		assertNotNull(ReferenceContainer.service.stringValue());
+		assertNotNull("System.out should be not null", System.out);
+		assertNotNull("System.err should be not null", System.err);
 
 	}
 
-	private void startDependency(Bundle simpleService2Bundle) throws BundleException, InterruptedException {
-		System.out.println("Starting dependency");
-		simpleService2Bundle.start();
 
-		waitOnContextCreation("org.eclipse.gemini.blueprint.iandt.simpleservice2");
-
-		System.out.println("Dependency started");
-	}
-
-	protected List getIAndTPermissions() {
-		List perms = super.getIAndTPermissions();
-		// export package
-		perms.add(new PropertyPermission("*", "read"));
-		perms.add(new PropertyPermission("*", "write"));
-		return perms;
-	}
-
-	protected List getTestPermissions() {
-		List perms = super.getTestPermissions();
-		perms.add(new FilePermission("<<ALL FILES>>", "read"));
-		perms.add(new AdminPermission("*", AdminPermission.EXECUTE));
-		perms.add(new AdminPermission("*", AdminPermission.LIFECYCLE));
-		perms.add(new AdminPermission("*", AdminPermission.RESOLVE));
-		perms.add(new PropertyPermission("*", "read"));
-		perms.add(new PropertyPermission("*", "write"));
-		return perms;
-	}
 }
