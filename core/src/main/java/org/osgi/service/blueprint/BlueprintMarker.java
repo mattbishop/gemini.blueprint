37b6f2374a5c98b47ef4f3b9209fa1562b6604ce
mbishop <matthew.bishop@elasticpath.com>
2012-11-16 16:06:41 -0800

    Fix for https://bugs.eclipse.org/bugs/show_bug.cgi?id=351755

---
+++ core/src/main/java/org/osgi/service/blueprint/BlueprintMarker.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright © Elastic Path Software Inc., 2012
+ */
+package org.osgi.service.blueprint;
+
+/**
+ * Marker class to trigger the export of package "org.osgi.service.blueprint" in the blueprint bundle,
+ * as per the R4.2 Spec Section 121.13.5 Type Compatibility
+ */
+public final class BlueprintMarker {
+	private BlueprintMarker() {
+		//not instantiated
+	}
+}
