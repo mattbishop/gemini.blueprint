fc045543b310dc5d6c5658a0759337b51ce09d5c
mbishop <matthew.bishop@elasticpath.com>
2012-11-21 13:45:50 -0800

    Added copyright and Licensing headers.

--- core/src/test/java/org/eclipse/gemini/blueprint/blueprint/container/TypeFactoryTest.java
+++ core/src/test/java/org/eclipse/gemini/blueprint/blueprint/container/TypeFactoryTest.java
@@ -1,5 +1,5 @@
 /******************************************************************************
- * Copyright (c) 2006, 2010 VMware Inc.
+ * Copyright (c) 2006, 2010 VMware Inc.; 2012 Elastic Path, Inc.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * and Apache License v2.0 which accompanies this distribution.
@@ -10,6 +10,7 @@
  *
  * Contributors:
  *   VMware Inc.
+ *   Elastic Path, Inc.
  *****************************************************************************/
 
 package org.eclipse.gemini.blueprint.blueprint.container;
